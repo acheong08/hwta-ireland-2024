bdf08d42d37e832fe5b45ffc79864c3b912f35ad
Antonio Cheong <acheong@duti.dev>
2024-08-20 16:14:36 +0800

    first code that actually runs without exceptions: fix type errors and shit

--- constants.py
+++ constants.py
@@ -1,8 +1,6 @@
 # pyright: basic
 
 
-from re import IGNORECASE
-
 import pandas as pd
 
 from solver.models import Datacenter, SellingPrices, Server
@@ -26,9 +24,12 @@ def get_servers() -> list[Server]:
 def get_selling_prices() -> list[SellingPrices]:
     selling_prices = pd.read_csv("data/selling_prices.csv")
 
-    return [
+    p = [
         SellingPrices(**sp) for _, sp in selling_prices.iterrows()  # pyright: ignore[]
     ]
+    for i in range(0, len(p)):
+        p[i].selling_price = int(p[i].selling_price * 10)
+    return p
 
 
 if __name__ == "__main__":
---
+++ solver/debuggy.py
@@ -0,0 +1,21 @@
+# pyright: basic
+import functools
+import pdb
+import sys
+
+
+def debug_on(*exceptions):
+    if not exceptions:
+        exceptions = (AssertionError,)
+
+    def decorator(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except exceptions:
+                pdb.post_mortem(sys.exc_info()[2])
+
+        return wrapper
+
+    return decorator
