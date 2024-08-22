d37183e88779011f9c975261a9243e036971c3ec
Antonio Cheong <acheong@duti.dev>
2024-08-22 22:17:21 +0800

    ignore 0 timestep action, generate every tiny server, add example generate

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
-from .solver.models import Action, ServerGeneration, SolutionEntry
+from solver.models import Action, ServerGeneration, SolutionEntry
+from utils import save_solution  # type: ignore[import]
 
 
 def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
@@ -10,7 +11,7 @@ def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
         if entry.action == Action.BUY:
             if ids.get(entry.datacenter_id) is None:
                 ids[entry.datacenter_id] = {entry.server_generation: []}
-            for _ in range(entry.amount):
+            for _ in range(entry.amount * 1000):
                 ids[entry.datacenter_id][entry.server_generation].append(counter)
                 solution.append(
                     {
@@ -23,7 +24,7 @@ def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
                 )
                 counter += 1
         elif entry.action == Action.DISMISS:
-            for _ in range(entry.amount):
+            for _ in range(entry.amount * 1000):
                 server_id = ids[entry.datacenter_id][entry.server_generation].pop(0)
                 solution.append(
                     {
@@ -35,3 +36,15 @@ def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
                     }
                 )
     return solution
+
+
+if __name__ == "__main__":
+    entries = [
+        SolutionEntry(1, "dc1", ServerGeneration.GPU_S1, Action.BUY, 10),
+        SolutionEntry(1, "dc2", ServerGeneration.GPU_S2, Action.BUY, 3),
+        SolutionEntry(2, "dc1", ServerGeneration.GPU_S1, Action.DISMISS, 5),
+        SolutionEntry(2, "dc2", ServerGeneration.GPU_S2, Action.DISMISS, 3),
+        SolutionEntry(3, "dc1", ServerGeneration.GPU_S1, Action.DISMISS, 5),
+    ]
+
+    save_solution(generate(entries), "data/test.json")
