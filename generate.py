a246037438d6dfe6f80917beda8acc8cd6a839fc
Antonio Cheong <acheong@duti.dev>
2024-08-23 00:22:09 +0800

    generate solution: fix life expectancy (pop until no expired)

--- generate.py
+++ generate.py
@@ -1,11 +1,15 @@
-from solver.models import Action, ServerGeneration, SolutionEntry
+from constants import get_servers
+from solver.models import Action, Server, ServerGeneration, SolutionEntry
 from utils import save_solution  # type: ignore[import]
 
 
-def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
+def generate(
+    entries: list[SolutionEntry], servers: list[Server]
+) -> list[dict[str, str | int]]:
+    server_map = {server.server_generation: server for server in servers}
     solution: list[dict[str, str | int]] = []
     # datacenter_id -> server_generation -> list[server id]
-    ids: dict[str, dict[ServerGeneration, list[int]]] = {}
+    ids: dict[str, dict[ServerGeneration, list[dict[str, int]]]] = {}
     counter = 0
     for entry in entries:
         if entry.action == Action.BUY:
@@ -14,7 +18,13 @@ def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
             if ids[entry.datacenter_id].get(entry.server_generation) is None:
                 ids[entry.datacenter_id][entry.server_generation] = []
             for _ in range(entry.amount * 1000):
-                ids[entry.datacenter_id][entry.server_generation].append(counter)
+                ids[entry.datacenter_id][entry.server_generation].append(
+                    {
+                        "id": counter,
+                        "expires_at": entry.timestep
+                        + server_map[entry.server_generation].life_expectancy,
+                    }
+                )
                 solution.append(
                     {
                         "time_step": entry.timestep,
@@ -26,13 +36,19 @@ def generate(entries: list[SolutionEntry]) -> list[dict[str, str | int]]:
                 )
                 counter += 1
         elif entry.action == Action.DISMISS:
+            # Pop until we have no more expired servers
+            while (
+                ids[entry.datacenter_id][entry.server_generation][0]["expires_at"]
+                <= entry.timestep
+            ):
+                _ = ids[entry.datacenter_id][entry.server_generation].pop(0)
             for _ in range(entry.amount * 1000):
                 server_id = ids[entry.datacenter_id][entry.server_generation].pop(0)
                 solution.append(
                     {
                         "time_step": entry.timestep,
                         "datacenter_id": entry.datacenter_id,
-                        "server_id": server_id,
+                        "server_id": server_id["id"],
                         "server_generation": entry.server_generation.value,
                         "action": "dismiss",
                     }
@@ -49,4 +65,4 @@ if __name__ == "__main__":
         SolutionEntry(3, "dc1", ServerGeneration.GPU_S1, Action.DISMISS, 5),
     ]
 
-    save_solution(generate(entries), "data/test.json")
+    save_solution(generate(entries, get_servers()), "data/test.json")
