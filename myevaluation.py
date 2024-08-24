0b83ff93d3dcc8a6b9fd159c31c6f8dd293369e6
Antonio Cheong <acheong@duti.dev>
2024-08-24 22:40:04 +0800

    add debugging for evaluation

--- myevaluation.py
+++ myevaluation.py
@@ -4,36 +4,43 @@ import os
 import sys
 
 from evaluation import evaluation_function
+from solver.debuggy import debug_on  # type: ignore
 from utils import load_problem_data, load_solution
 
-# List files in output directory
-solutions = [
-    (
-        f"./{sys.argv[1] if len(sys.argv) == 2 else "output"}/{f}"
-        if f.endswith(".json")
-        else None
-    )
-    for f in os.listdir("output")
-]
 
-for f in solutions:
-    if not f:
-        continue
-    print(f"{f} - ", end="", flush=True)
-    # LOAD SOLUTION
-    solution = load_solution(f)
+@debug_on(Exception)
+def main():
+    # List files in output directory
+    solutions = [
+        (
+            f"./{sys.argv[1] if len(sys.argv) == 2 else "output"}/{f}"
+            if f.endswith(".json")
+            else None
+        )
+        for f in os.listdir(f"./{sys.argv[1] if len(sys.argv) == 2 else 'output'}")
+    ]
 
-    # LOAD PROBLEM DATA
-    demand, datacenters, servers, selling_prices = load_problem_data()
+    for f in solutions:
+        if not f:
+            continue
+        print(f"{f} - ", end="", flush=True)
+        # LOAD SOLUTION
+        solution = load_solution(f)
 
-    # EVALUATE THE SOLUTION
-    score = evaluation_function(
-        solution,
-        demand,
-        datacenters,
-        servers,
-        selling_prices,
-        seed=int(f.split("/")[-1].split(".")[0]),
-    )
+        # LOAD PROBLEM DATA
+        demand, datacenters, servers, selling_prices = load_problem_data()
 
-    print(f"Solution score: {score}")
+        # EVALUATE THE SOLUTION
+        score = evaluation_function(
+            solution,
+            demand,
+            datacenters,
+            servers,
+            selling_prices,
+            seed=int(f.split("/")[-1].split(".")[0]),
+        )
+
+        print(f"Solution score: {score}")
+
+
+main()
