553d83de8c814a0eda9468007e57776555b73ee7
aabdullah27182845 <abdullahnavid5@gmail.com>
2024-08-25 17:35:54 +0100

    current code, not completed

--- mock_solution.py
+++ mock_solution.py
@@ -12,6 +12,19 @@ a = np.array([100 for _ in range(21)])
 def calculate_partial_derivative(z, d, Z_list, D_list, p_list, iter):
     little_derivative = np.exp(-z) / (np.exp(-z) + np.exp(-d))
     x_encoding = [np.log(np.exp(-z)+np.exp(-d)) for z, d in zip(Z_list, D_list)]
+    total_derivative = sum(x_encoding)*little_derivative #not the correct function as of yet
+    return total_derivative
 
+def total_derivative(Z_list, D_list, p_list, iter):
+    total_derivative = []
+    for (i,j) in zip(Z_list, D_list):
+        total_derivative.append(calculate_partial_derivative(i, j, Z_list, D_list, p_list, iter))
+    total_derivative = np.array(total_derivative)
+    return total_derivative
 
+def gradient_descent(Z_list, D_list, p_list, iter):
+    total_derivative = total_derivative(Z_list, D_list, p_list, iter)
+    for i in range(10):
+        Z_list = Z_list - total_derivative
+    return Z_list
 
