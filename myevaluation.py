8c1b24600a7e6e4f2a7a82227999022cb7827ab5
Antonio Cheong <acheong@duti.dev>
2024-08-29 16:43:40 +0800

    fix evaluation - list dir

--- myevaluation.py
+++ myevaluation.py
@@ -6,15 +6,12 @@ import sys
 from evaluation import evaluation_function
 from utils import load_problem_data, load_solution
 
+thedir = sys.argv[1] if len(sys.argv) == 2 else "output"
 # List files in output directory
 solutions = [
-    (
-        f"./{sys.argv[1] if len(sys.argv) == 2 else "output"}/{f}"
-        if f.endswith(".json")
-        else None
-    )
-    for f in os.listdir("output")
+    (f"./{thedir}/{f}" if f.endswith(".json") else None) for f in os.listdir(thedir)
 ]
+solutions.reverse()
 
 total_score = 0
 for f in solutions:
@@ -27,6 +24,12 @@ for f in solutions:
     # LOAD PROBLEM DATA
     demand, datacenters, servers, selling_prices = load_problem_data()
 
+    seed = 0
+    fname = f.split("/")[-1]
+    if len(fname.split("_")) == 2:
+        seed = int(fname.split("_")[0])
+    else:
+        seed = int(fname.split(".")[0])
     # EVALUATE THE SOLUTION
     score: int = evaluation_function(  # type: ignore[]
         solution,
