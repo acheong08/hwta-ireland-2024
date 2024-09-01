b25d8df2c1804d069cc9848cd9ebdc27a0ce1577
Henrik Vlijter <Henrik.Vlijter@hotmail.com>
2024-09-01 21:12:33 +0100

    score not generating

--- rl.py
+++ rl.py
@@ -1,184 +1,195 @@
-import gym
 import numpy as np
-import torch
-import torch.nn as nn
-import tensorflow as tf
-import random
-from collections import deque
-import json
 import pandas as pd
-from scipy.stats import truncweibull_min
-import logging
+import tensorflow as tf
+from tensorflow import keras
+import uuid
+from evaluation import (
+    get_capacity_by_server_generation_latency_sensitivity,
+    get_normalized_lifespan,
+    get_profit,
+    get_time_step_demand, 
+    get_utilization,
+    get_known
+)   
 
-from evaluation import get_known, update_fleet, get_capacity_by_server_generation_latency_sensitivity, get_utilization, get_normalized_lifespan, get_profit, evaluation_function
-from constants import get_datacenters, get_selling_prices, get_servers, get_demand
-
-# env.reset() – To reset the entire environment and obtain the initial values of observation.
-# env.render() – Rendering the environment for displaying the visualization of the working setup.
-# env.step() – To proceed with an action on the selected environment.
-# env.close() – Close the particular render frame of the environment.
-logger = logging.getLogger()
-file_handler = logging.FileHandler('logs.log')
-logger.addHandler(file_handler)
-formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
-file_handler.setFormatter(formatter)
-
-# NN
 class ServerFleetEnvironment:
-    def __init__(self, datacenters, servers, selling_prices, demand):
-        self.datacenters = datacenters
-        self.servers = servers
-        self.selling_prices = selling_prices
-        self.demand = demand
+    def __init__(self, datacenters_file, demand_file, selling_prices_file, servers_file):
+        self.datacenters = pd.read_csv(datacenters_file)
+        self.demand = pd.read_csv(demand_file)
+        self.selling_prices = pd.read_csv(selling_prices_file)
+        self.servers = pd.read_csv(servers_file)
+        
         self.time_step = 1
-        self.fleet = pd.DataFrame()
+        self.max_steps = self.demand['time_step'].max()
+        
+        self.server_types = get_known("server_generation")
+        self.datacenter_ids = get_known("datacenter_id")
+        
+        self.fleet = self.initialize_fleet()
+        self.actions = []
+
+    def initialize_fleet(self):
+        return {dc: {st: [] for st in self.server_types} for dc in self.datacenter_ids}
 
     def reset(self):
         self.time_step = 1
-        self.fleet = pd.DataFrame()
-        return self.get_state()
+        self.fleet = self.initialize_fleet()
+        self.actions = []
+        return self._get_state()
 
     def step(self, action):
-        self.apply_action(action)
-        next_state = self.get_state()
-        reward = self.calculate_reward()
-        done = self.time_step >= get_known('time_steps')
+        self.update_fleet(action)
+        reward = self.get_profit()
+        state = self._get_state()
         self.time_step += 1
-        return next_state, reward, done
+        done = self.time_step > self.max_steps
+        return state, reward, done, {}
 
-    def get_state(self):
-        demand_ts = get_time_step_demand(self.demand, self.time_step)
-        demand_array = demand_ts.values
-        state = {
-            'time_step': self.time_step,
-            'fleet': self.fleet.to_dict('records') if not self.fleet.empty else [],
-            'demand': demand_array.tolist()
-        }
-        return state
-
-    def apply_action(self, action):
-        self.fleet = update_fleet(self.time_step, self.fleet, action)
-
-    def calculate_reward(self):
-        D = get_time_step_demand(self.demand, self.time_step)
-        Z = get_capacity_by_server_generation_latency_sensitivity(self.fleet)
-        U = get_utilization(D, Z)
-        L = get_normalized_lifespan(self.fleet)
-        P = get_profit(D, Z, self.selling_prices, self.fleet)
-        return U * L * P
-
-class DQN(tf.keras.Model):
-    def __init__(self, state_size, action_size):
-        super(DQN, self).__init__()
-        self.dense1 = tf.keras.layers.Dense(128, activation='relu')
-        self.dense2 = tf.keras.layers.Dense(128, activation='relu')
-        self.dense3 = tf.keras.layers.Dense(action_size)
-
-    def call(self, state):
-        # Process time_step
-        time_step = tf.cast(state['time_step'], tf.float32)
+    def _get_state(self):
+        current_demand = self._get_current_demand()
+        current_fleet = self.get_time_step_fleet()
+        datacenter_capacity = self.datacenters['slots_capacity'].values
         
-        # Process fleet
-        fleet = state['fleet']
-        if len(fleet) == 0:
-            fleet_tensor = tf.zeros((1, 5))  # Assuming 5 features for each server
-        else:
-            fleet_tensor = tf.convert_to_tensor([[
-                server['time_step'],
-                self.encode_datacenter(server['datacenter_id']),
-                self.encode_server_generation(server['server_generation']),
-                self.encode_server_id(server['server_id']),
-                self.encode_action(server['action'])
-            ] for server in fleet], dtype=tf.float32) # Use float 32 on cpu and 16 on cuda
+        state = []
+        for dc in self.datacenter_ids:
+            for st in self.server_types:
+                state.append(len(current_fleet[dc][st]))
         
-        # Process demand
-        demand = tf.convert_to_tensor(state['demand'], dtype=tf.float32)
-        
-        # Combine all inputs
-        combined_input = tf.concat([
-            tf.expand_dims(time_step, -1),
-            tf.reshape(fleet_tensor, (-1,)),
-            tf.reshape(demand, (-1,))
-        ], axis=-1)
-        
-        x = self.dense1(combined_input)
-        x = self.dense2(x)
-        return self.dense3(x)
+        return np.array(state + list(current_demand) + list(datacenter_capacity))
 
-def train_model(env, model, episodes, epsilon=0.1):
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    loss_fn = tf.keras.losses.MeanSquaredError()
+    def _get_current_demand(self):
+        demand_at_ts = self.demand[self.demand['time_step'] == self.time_step]
+        if demand_at_ts.empty:
+            return [0] * len(self.server_types)
+        demand_row = demand_at_ts.iloc[0]
+        return [demand_row.get(st, 0) for st in self.server_types]
 
+    def get_time_step_fleet(self):
+        return self.fleet
+
+    def update_fleet(self, action):
+        dc_index, st_index = divmod(action, len(self.server_types))
+        datacenter = self.datacenter_ids[dc_index]
+        server_type = self.server_types[st_index]
+        
+        if len(self.fleet[datacenter][server_type]) < self.datacenters.loc[self.datacenters['datacenter_id'] == datacenter, 'slots_capacity'].values[0]:
+            server_id = str(uuid.uuid4())
+            self.fleet[datacenter][server_type].append(server_id)
+            self.actions.append({
+                "time_step": self.time_step,
+                "datacenter_id": datacenter,
+                "server_generation": server_type,
+                "server_id": server_id,
+                "action": "buy"
+            })
+
+    def get_profit(self):
+        total_capacity = sum(len(servers) for dc in self.fleet.values() for servers in dc.values())
+        total_demand = sum(self._get_current_demand())
+        utilization = min(total_capacity, total_demand) / total_capacity if total_capacity > 0 else 0
+        return utilization * 100  # Scale the reward for better learning
+
+class PPOAgent:
+    def __init__(self, state_dim, action_dim, lr=0.0003):
+        self.state_dim = state_dim
+        self.action_dim = action_dim
+        self.lr = lr
+        self.gamma = 0.99
+        self.clip_ratio = 0.2
+        self.actor = self._build_actor()
+        self.critic = self._build_critic()
+        self.actor_optimizer = keras.optimizers.Adam(lr)
+        self.critic_optimizer = keras.optimizers.Adam(lr)
+
+    def _build_actor(self):
+        inputs = keras.layers.Input(shape=(self.state_dim,))
+        x = keras.layers.Dense(64, activation='relu')(inputs)
+        x = keras.layers.Dense(64, activation='relu')(x)
+        outputs = keras.layers.Dense(self.action_dim, activation='softmax')(x)
+        return keras.Model(inputs, outputs)
+
+    def _build_critic(self):
+        inputs = keras.layers.Input(shape=(self.state_dim,))
+        x = keras.layers.Dense(64, activation='relu')(inputs)
+        x = keras.layers.Dense(64, activation='relu')(x)
+        outputs = keras.layers.Dense(1)(x)
+        return keras.Model(inputs, outputs)
+
+    def get_action(self, state):
+        probs = self.actor.predict(np.array([state]), verbose=0)[0]
+        return np.random.choice(self.action_dim, p=probs)
+
+    def update(self, states, actions, advantages, old_probs, returns):
+        states = np.array(states)
+        actions = np.array(actions)
+        advantages = np.array(advantages)
+        old_probs = np.array(old_probs)
+        returns = np.array(returns)
+
+        with tf.GradientTape() as tape:
+            new_probs = self.actor(states, training=True)
+            new_probs = tf.gather_nd(new_probs, tf.stack([tf.range(actions.shape[0]), actions], axis=1))
+            ratio = new_probs / old_probs
+            clip_ratio = tf.clip_by_value(ratio, 1 - self.clip_ratio, 1 + self.clip_ratio)
+            surrogate = tf.minimum(ratio * advantages, clip_ratio * advantages)
+            actor_loss = -tf.reduce_mean(surrogate)
+
+        actor_grads = tape.gradient(actor_loss, self.actor.trainable_variables)
+        self.actor_optimizer.apply_gradients(zip(actor_grads, self.actor.trainable_variables))
+
+        with tf.GradientTape() as tape:
+            value_pred = self.critic(states, training=True)
+            critic_loss = tf.reduce_mean(tf.square(returns - value_pred))
+
+        critic_grads = tape.gradient(critic_loss, self.critic.trainable_variables)
+        self.critic_optimizer.apply_gradients(zip(critic_grads, self.critic.trainable_variables))
+
+
+def train(env, agent, episodes=1000, batch_size=32):
     for episode in range(episodes):
         state = env.reset()
-        total_reward = 0
+        ep_rewards = []
+        states, actions, rewards, values, probs = [], [], [], [], []
+
         done = False
-
         while not done:
-            if np.random.random() < epsilon:
-                action = np.random.randint(0, model.output.shape[1])
-            else:
-                q_values = model(state)
-                action = np.argmax(q_values[0])
-
-            next_state, reward, done = env.step(action)
-            total_reward += reward
-
-            with tf.GradientTape() as tape:
-                q_values = model(state)
-                next_q_values = model(next_state)
-                target = reward + 0.99 * np.max(next_q_values[0])
-                loss = loss_fn(target, q_values[0][action])
-
-            grads = tape.gradient(loss, model.trainable_variables)
-            optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
+            action = agent.get_action(state)
+            next_state, reward, done, _ = env.step(action)
+            
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            values.append(agent.critic.predict(np.array([state]), verbose=0)[0][0])
+            probs.append(agent.actor.predict(np.array([state]), verbose=0)[0][action])
+            
+            ep_rewards.append(reward)
             state = next_state
 
-        print(f"Episode {episode + 1}, Total Reward: {total_reward}")
+            if len(states) >= batch_size or done:
+                returns = []
+                advantages = []
+                R = 0
+                for r, v in zip(rewards[::-1], values[::-1]):
+                    R = r + agent.gamma * R
+                    returns.insert(0, R)
+                    advantages.insert(0, R - v)
+                
+                agent.update(states, actions, advantages, probs, returns)
+                states, actions, rewards, values, probs = [], [], [], [], []
 
-    return model
+        if episode % 10 == 0:
+            print(f"Episode {episode}, Average Reward: {np.mean(ep_rewards)}")
 
-def evaluate_and_generate_solution(model, env, seed):
-    np.random.seed(seed)
-    state = env.reset()
-    solution = []
+    return env.actions
 
-    for t in range(get_known('time_steps')):
-        q_values = model(state)
-        action = np.argmax(q_values[0])
+# Usage
+env = ServerFleetEnvironment('data/datacenters.csv', 'data/demand.csv', 'data/selling_prices.csv', 'data/servers.csv')
+state_dim = len(env._get_state())
+action_dim = len(env.server_types) * len(env.datacenter_ids)
+agent = PPOAgent(state_dim, action_dim)
+actions = train(env, agent, episodes=1000)
 
-        action_dict = {
-            'time_step': t + 1,
-            'datacenter_id': get_known('datacenter_id')[action % len(get_known('datacenter_id'))],
-            'server_generation': get_known('server_generation')[action // len(get_known('datacenter_id'))],
-            'server_id': f"server_{t}_{action}",
-            'action': 'buy'
-        }
-        solution.append(action_dict)
-
-        next_state, _, done = env.step(action)
-        state = next_state
-        if done:
-            break
-
-    solution_df = pd.DataFrame(solution)
-    return solution_df
-
-def get_time_step_demand(demand, ts):
-    # GET THE DEMAND AT A SPECIFIC TIME-STEP t
-    return demand[demand['time_step'] == ts]
-
-if __name__ == "__main__":
-    datacenters = get_datacenters()
-    servers = get_servers()
-    selling_prices = get_selling_prices()
-    demand = get_demand()
-
-    print(datacenters)
-    print("-----------------------------------------------------------------------------------------------")
-    print(servers)
-    print("-----------------------------------------------------------------------------------------------")
-    print(selling_prices)
-    print("-----------------------------------------------------------------------------------------------")
-    print(demand)
+# Save actions to a file
+import json
+with open('actions.json', 'w') as f:
+    json.dump(actions, f, indent=2)
