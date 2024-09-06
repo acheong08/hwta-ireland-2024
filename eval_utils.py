f1f478f73e4267fd5ef8d7aed9f267693c284223
Antonio Cheong <acheong@duti.dev>
2024-09-07 05:01:54 +0800

    remove md5 cache because fuck windows

--- eval_utils.py
+++ eval_utils.py
@@ -1,6 +1,5 @@
 # pyright: basic
 import json
-import subprocess
 
 import numpy as np
 
@@ -12,13 +11,6 @@ from utils import load_problem_data, load_solution
 
 
 def get_score(f: str, seed: int, rerun: bool = False, fast: bool = True) -> float:
-    solution_scores: dict[str, float] = json.load(open("solution_scores.json", "r"))
-    md5sum = subprocess.run(
-        ["md5sum", f], stdout=subprocess.PIPE, text=True
-    ).stdout.split()[0]
-    if not rerun:
-        if md5sum in solution_scores:
-            return solution_scores[md5sum]
     np.random.seed(seed)
     score = 0
     if fast:
@@ -38,6 +30,4 @@ def get_score(f: str, seed: int, rerun: bool = False, fast: bool = True) -> floa
         score: float = evaluation_function(  # type: ignore[reportAssignmentType]
             solution, demand, datacenters, servers, selling_prices, seed=seed
         )
-    solution_scores[md5sum] = score
-    json.dump(solution_scores, open("solution_scores.json", "w"))
     return score
